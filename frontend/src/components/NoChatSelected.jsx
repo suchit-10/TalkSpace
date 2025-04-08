4a6d334fd36405f3d6081f42771e9bef06263ded
Suchit <gourkhedesuchit@gmail.com>
2025-04-08 11:43:48 +0530

    final

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -19,8 +19,7 @@ const NoChatSelected = () => {
         {/* Welcome Text */}
         <h2 className="text-2xl font-bold">Welcome to TalkSpace!</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting powered by
-          <span className="text-primary font-semibold">Suchit Gourkhede</span>
+        Select a conversation from the sidebar to start chatting powered by <span className="font-semibold">Suchit Gourkhede</span>.
         </p>
       </div>
     </div>
